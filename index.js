c22338d2e0c6b8531dd23031af2e216eec504009
kacha930 <robiwere20@gmail.com>
2024-07-22 07:29:07 +0300

    success

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+document.querySelector('main#main').remove();
+
+//store
+let newHeader = document.createElement('h1');
+
+newHeader.id = "victory";
+// html content
+newHeader.innerHTML = "rabanne is the champion";
+document.body.appendChild(newHeader);
